008e36f8528a62b87f608892d10bd4f2a6e7f333
Szczoor <mateusz.gorniak8@gmail.com>
2026-04-01 15:38:01 +0200

    Dodanie klasy Sasiad i możliwość dodania sąsiada w wierzchołku Generowanie polaczen

--- Generator.py
+++ Generator.py
@@ -1,8 +1,12 @@
 from random import randint, choice
+from typing import List
+from math import dist
+
 import KlasyGrafu as Graf
 
-domki = []
-kopalnie = []
+domki: List[Graf.Domek] = []
+kopalnie: List[Graf.Kopalnia] = []
+
 
 for siatka_x in range (0, 10):
     for siatka_y in range(0, 10):
@@ -17,3 +21,17 @@ for siatka_x in range (0, 10):
             nowy_kopalnia = Graf.Kopalnia(x, y, randint(1, 4), choice(Graf.materialy))
             kopalnie.append(nowy_kopalnia)
 
+
+for polacz_domek in domki:
+    for polacz_kopalnie in kopalnie:
+
+        odleglosc = dist((polacz_domek.x, polacz_domek.y), (polacz_kopalnie.x, polacz_kopalnie.y))
+
+        polacz_domek.dodaj_sasiada(polacz_kopalnie, odleglosc)
+        polacz_kopalnie.dodaj_sasiada(polacz_domek, odleglosc)
+
+
+# Sasiedzi pierwszego domka
+print(domki[0])
+for domek0 in domki[0].sasiedzi:
+    print(domek0)
--- KlasyGrafu.py
+++ KlasyGrafu.py
@@ -1,16 +1,33 @@
+from typing import List
+
 materialy = ["ZLOTO", "DIAMENTY", "WEGIEL", "REDSTONE", "LAPIS", "ZELAZO"]
+
+
+class Sasiad:
+
+    def __init__(self, node: NodeGrafu, odleglosc: float):
+        self.node = node
+        self.odleglosc = odleglosc
+
+    def __str__(self):
+        return f"{self.node} w odleglosci {self.odleglosc}"
+
 
 class NodeGrafu:
 
-    def __init__(self, x, y):
+    def __init__(self, x: float, y: float):
         self.x = x
         self.y = y
-        self.sasiedzi = []
+        self.sasiedzi: List[Sasiad] = []
+
+    def dodaj_sasiada(self, sasiad: NodeGrafu, odleglosc: float):
+        nowy_sasiad = Sasiad(sasiad, odleglosc)
+        self.sasiedzi.append(nowy_sasiad)
 
 
 class Domek(NodeGrafu):
 
-    def __init__(self, x, y, preferencja):
+    def __init__(self, x: float, y: float, preferencja: str):
         super().__init__(x, y)
         self.preferencja = preferencja
 
@@ -20,7 +37,7 @@ class Domek(NodeGrafu):
 
 class Kopalnia(NodeGrafu):
 
-    def __init__(self, x, y, pojemnosc, zloze):
+    def __init__(self, x: int, y: int, pojemnosc: int, zloze: str):
         super().__init__(x, y)
         self.pojemnosc = pojemnosc
         self.zloze = zloze
