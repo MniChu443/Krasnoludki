e833c3bb8098fe41d530ecd0a4cc15ac99a1fade
Michal <michaljeziorski876@gmail.com>
2026-06-02 20:01:43 +0200

    Zaimplementowanie DrzewaPrzedzialowego

---
+++ src/algorytmy/drzewo_przedzialowe.py
@@ -0,0 +1,66 @@
+from typing import List, Tuple, Optional
+
+class SegmentTree:
+   
+
+    def __init__(self, data: List[int]):
+        
+        self.n = len(data)
+        # Drzewo przechowuje krotki (maksymalna_wartość, indeks_w_oryginalnej_tablicy)
+        # Optymalny rozmiar tablicy dla drzewa przedziałowego to 4 * n
+        self.tree: List[Optional[Tuple[int, int]]] = [None] * (4 * self.n)
+        if self.n > 0:
+            self._build(data, 0, 0, self.n - 1)
+
+    def _build(self, data: List[int], node: int, start: int, end: int):
+        
+        if start == end:
+            # Węzeł-liść, przypisujemy mu wartość i indeks z tablicy początkowej
+            self.tree[node] = (data[start], start)
+        else:
+            mid = (start + end) // 2
+            left_child = 2 * node + 1
+            right_child = 2 * node + 2
+            
+            # Budowa poddrzew
+            self._build(data, left_child, start, mid)
+            self._build(data, right_child, mid + 1, end)
+            
+            # Węzeł wewnętrzny przyjmuje maksimum ze swoich dzieci
+            left_val = self.tree[left_child]
+            right_val = self.tree[right_child]
+            
+            if left_val[0] >= right_val[0]:
+                self.tree[node] = left_val
+            else:
+                self.tree[node] = right_val
+
+    def query(self, left: int, right: int) -> Tuple[int, int]:
+       
+        if left < 0 or right >= self.n or left > right:
+            raise ValueError("Nieprawidłowe granice zapytania o przedział")
+            
+        return self._query_recursive(0, 0, self.n - 1, left, right)
+
+    def _query_recursive(self, node: int, start: int, end: int, left: int, right: int) -> Tuple[int, int]:
+        
+        # 1. Przedział węzła zawiera się całkowicie w zapytaniu
+        if left <= start and right >= end:
+            return self.tree[node]
+            
+        # 2. Przedział węzła znajduje się poza przedziałem zapytania
+        if end < left or start > right:
+            return (-float('inf'), -1)
+            
+        # 3. Częściowe pokrycie przedziałów, konieczne wejście głębiej do dzieci węzła
+        mid = (start + end) // 2
+        left_child = 2 * node + 1
+        right_child = 2 * node + 2
+        
+        left_res = self._query_recursive(left_child, start, mid, left, right)
+        right_res = self._query_recursive(right_child, mid + 1, end, left, right)
+        
+        if left_res[0] >= right_res[0]:
+            return left_res
+        else:
+            return right_res
--- src/main.py
+++ src/main.py
@@ -3,9 +3,9 @@ from narzedzia import obsluga_json as ObslugaJSON
 from algorytmy import ksiaze_algorytm as KsiazeAlgorytm
 from narzedzia import drukarnia as Drukarnia
 from modele import klasy_grafu as KlasyGrafu
+from algorytmy.drzewo_przedzialowe import SegmentTree
 
-
-# Generowanie / wczytanie grafu
+# Generowanie i wczytanie grafu
 graf = Generator.wygeneruj_graf(5, 5)
 ObslugaJSON.zapisz_do_pliku(graf, "../data/wyjscie/test.json")
 graf = ObslugaJSON.wczytaj_plik("../data/wyjscie/test.json")
@@ -13,7 +13,7 @@ Drukarnia.pokaz_statystyki(graf)
 Drukarnia.pokaz_domki(graf)
 Drukarnia.pokaz_kopalnie(graf)
 
-# Trasa księcia
+# Wyznaczanie trasy księcia
 kopalnie = [w for w in graf if type(w) is KlasyGrafu.Kopalnia]
 
 if kopalnie:
@@ -22,3 +22,15 @@ if kopalnie:
     Drukarnia.pokaz_otoczke(trasa, dlugosc)
 else:
     print("  Brak kopalni - trasa niemożliwa do wyznaczenia.")
+
+print("\n--- Najgłośniejszy krasnoludek ---")
+
+# RMQ - Drzewo przedziałowe
+glosnosci = [10, 22, 15, 30, 8, 12, 25, 18]
+print(f"Głośności krasnoludków: {glosnosci}")
+
+drzewo = SegmentTree(glosnosci)
+lewy, prawy = 1, 4
+max_glosnosc, indeks = drzewo.query(lewy, prawy)
+
+print(f"Zapytanie o przedział [{lewy}, {prawy}]: Maksymalna głośność = {max_glosnosc} (indeks {indeks}).")
