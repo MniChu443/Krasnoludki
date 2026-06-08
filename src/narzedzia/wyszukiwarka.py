e7cfa211950a8e9cfef48c66c2e1f261ec0c1513
mega-kalisz <kubakaliski@o2.pl>
2026-06-08 19:46:43 +0200

    Poprawiona wyszukiwarka tesktu

--- src/narzedzia/wyszukiwarka.py
+++ src/narzedzia/wyszukiwarka.py
@@ -4,16 +4,17 @@
 from algorytmy import Boyer_i_Moore
 
 def szukaj(plik, fraza):
-    opened = open(plik, "r")
     znalezione = []
     suma_znakow = 0
-    while True:
-        linia = opened.readline()
-        if not linia:
-            opened.close()
-            break
-        indeks = Boyer_i_Moore.boyer_moore(linia, fraza)
-        if indeks != -1:
-            znalezione.append(indeks + suma_znakow)
-        suma_znakow += len(linia)
+
+    with open(plik, "r", encoding="utf-8") as opened:
+        for linia in opened:
+            start = 0
+            while start < len(linia):
+                indeks = Boyer_i_Moore.boyer_moore(linia[start:], fraza)
+                if indeks == -1:
+                    break
+                znalezione.append(suma_znakow + start + indeks)
+                start += indeks + 1
+            suma_znakow += len(linia)
     return znalezione
