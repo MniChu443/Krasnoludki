f1254c800de60b26c97cfb8ac53cea3dce45310e
mega-kalisz <kubakaliski@o2.pl>
2026-06-10 13:00:27 +0200

    Prototyp algorytmu huffmana

--- src/algorytmy/kodowanie_Huffmana.py
+++ src/algorytmy/kodowanie_Huffmana.py
@@ -1,1 +1,51 @@
-#plik do algorytmu huffmana do kompresji danych
+#prototyp drzewa huffmana
+
+string = 'ZATERKOTAL KRUK W TARTAKU I TRAKTOR GO WYKARASKAL TRRR'
+
+
+class NodeTree(object):
+
+    def __init__(self, left=None, right=None):
+        self.left = left
+        self.right = right
+
+    def children(self):
+        return (self.left, self.right)
+
+    def __str__(self):
+        return '%s_%s' % (self.left, self.right)
+
+def drzewo_huffmana(node, left=True, binString=''):
+    if type(node) is str:
+        return {node: binString}
+    (l, r) = node.children()
+    drzewiec = dict()
+    drzewiec.update(drzewo_huffmana(l, True, binString + '0'))
+    drzewiec.update(drzewo_huffmana(r, False, binString + '1'))
+    return drzewiec
+
+
+freq = {}
+for c in string:
+    if c in freq:
+        freq[c] += 1
+    else:
+        freq[c] = 1
+
+freq = sorted(freq.items(), key=lambda x: x[1], reverse=True)
+
+nodes = freq
+
+while len(nodes) > 1:
+    (key1, c1) = nodes[-1]
+    (key2, c2) = nodes[-2]
+    nodes = nodes[:-2]
+    node = NodeTree(key1, key2)
+    nodes.append((node, c1 + c2))
+
+    nodes = sorted(nodes, key=lambda x: x[1], reverse=True)
+
+kod = drzewo_huffmana(nodes[0][0])
+
+for (char, frequency) in freq:
+    print(' %-4r |%12s' % (char, kod[char]))
