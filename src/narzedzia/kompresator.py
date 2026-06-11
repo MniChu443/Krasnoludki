76e22329243a8dbde12d6adafc9e87d896876c53
mega-kalisz <kubakaliski@o2.pl>
2026-06-11 15:44:56 +0200

    Huffman nauczyl sie dekompresji

--- src/narzedzia/kompresator.py
+++ src/narzedzia/kompresator.py
@@ -3,8 +3,8 @@ import json
 
 
 def kompresja(plik_wejsciowy, plik_wyjsciowy):
-    with open(plik_wejsciowy, 'r', encoding='utf-8') as f:
-        tekst = f.read()
+    with open(plik_wejsciowy, 'r', encoding='utf-8') as plik:
+        tekst = plik.read()
 
     kody = Huff.huffman(tekst)
 
@@ -17,6 +17,39 @@ def kompresja(plik_wejsciowy, plik_wyjsciowy):
         'zakodowany_tekst': zakodowany_tekst
     }
 
-    with open(plik_wyjsciowy, 'w', encoding='utf-8') as f:
-        json.dump(dane, f, ensure_ascii=False)
+    with open(plik_wyjsciowy, 'w', encoding='utf-8') as plik:
+        json.dump(dane, plik, ensure_ascii=False)
 
+
+def odkoduj_plik_huffmanem(plik_wejsciowy, plik_wyjsciowy):
+    with open(plik_wejsciowy, 'r', encoding='utf-8') as plik:
+        dane = json.load(plik)
+
+    kody = dane['kody']
+    zakodowany = dane['zakodowany_tekst']
+
+    odwrotne_kody = {kod: znak for znak, kod in kody.items()}
+
+    if len(odwrotne_kody) == 0:
+        with open(plik_wyjsciowy, 'w', encoding='utf-8') as plik:
+            plik.write('')
+        return
+
+    if len(odwrotne_kody) == 1:
+        znak = list(odwrotne_kody.values())[0]
+        odkodowany = znak * len(zakodowany)
+        with open(plik_wyjsciowy, 'w', encoding='utf-8') as plik:
+            plik.write(odkodowany)
+        return
+
+    aktualny_kod = ''
+    odkodowany = ''
+
+    for bit in zakodowany:
+        aktualny_kod += bit
+        if aktualny_kod in odwrotne_kody:
+            odkodowany += odwrotne_kody[aktualny_kod]
+            aktualny_kod = ''
+
+    with open(plik_wyjsciowy, 'w', encoding='utf-8') as plik:
+        plik.write(odkodowany)
