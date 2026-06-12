42385a3101f39043dc556f4e101def456f248c8e
mega-kalisz <kubakaliski@o2.pl>
2026-06-12 12:16:01 +0200

    Dodanie obslugi kompresatora

--- src/narzedzia/kompresator.py
+++ src/narzedzia/kompresator.py
@@ -2,7 +2,7 @@ from algorytmy import kodowanie_Huffmana as Huff
 import json
 
 #funkcja kompresowania uzywajaca kodowania Huffmana
-def kompresja(plik_wejsciowy, plik_wyjsciowy):
+def kompresja(plik_wejsciowy):
     with open(plik_wejsciowy, 'r', encoding='utf-8') as plik:
         tekst = plik.read()
 
@@ -12,33 +12,30 @@ def kompresja(plik_wejsciowy, plik_wyjsciowy):
     for znak in tekst:
         zakodowany_tekst += kody[znak]
 
-    dane = {
-        'kody': kody,
-        'zakodowany_tekst': zakodowany_tekst
-    }
 
-    with open(plik_wyjsciowy, 'w', encoding='utf-8') as plik:
-        json.dump(dane, plik, ensure_ascii=False)
+    with open(plik_wejsciowy + ".huff", 'w', encoding='utf-8') as plik:
+        plik.write(zakodowany_tekst)
+    with open(plik_wejsciowy + ".kody", 'w', encoding='utf-8') as plik:
+        json.dump(kody, plik, ensure_ascii=False, indent=4)
 
 #funkcja dekompresowania uzywajaca kodowania Huffmana
-def dekompresja(plik_wejsciowy, plik_wyjsciowy):
-    with open(plik_wejsciowy, 'r', encoding='utf-8') as plik:
-        dane = json.load(plik)
-
-    kody = dane['kody']
-    zakodowany = dane['zakodowany_tekst']
+def dekompresja(nowy_plik, plik_z_kodami, plik_zakodowany):
+    with open(plik_z_kodami, 'r', encoding='utf-8') as plik:
+        kody = json.load(plik)
+    with open(plik_zakodowany, 'r', encoding='utf-8') as plik:
+        zakodowany = plik.read()
 
     odwrotne_kody = {kod: znak for znak, kod in kody.items()}
 
     if len(odwrotne_kody) == 0:
-        with open(plik_wyjsciowy, 'w', encoding='utf-8') as plik:
+        with open(nowy_plik, 'w', encoding='utf-8') as plik:
             plik.write('')
         return
 
     if len(odwrotne_kody) == 1:
         znak = list(odwrotne_kody.values())[0]
         odkodowany = znak * len(zakodowany)
-        with open(plik_wyjsciowy, 'w', encoding='utf-8') as plik:
+        with open(nowy_plik, 'w', encoding='utf-8') as plik:
             plik.write(odkodowany)
         return
 
@@ -50,6 +47,8 @@ def dekompresja(plik_wejsciowy, plik_wyjsciowy):
         if aktualny_kod in odwrotne_kody:
             odkodowany += odwrotne_kody[aktualny_kod]
             aktualny_kod = ''
+    if aktualny_kod != '':
+        raise ValueError("Plik nie jesy poprawnie zakodowany")
 
-    with open(plik_wyjsciowy, 'w', encoding='utf-8') as plik:
+    with open(nowy_plik, 'w', encoding='utf-8') as plik:
         plik.write(odkodowany)
---
+++ src/narzedzia/obsluga_kompresatora.py
@@ -0,0 +1,13 @@
+import kompresator
+
+opcja = input("Co zamierzasz?\n 1. Skompresowac plik\n 2. Dekompresowac plik\n 0. Wyjscie\n")
+if opcja == "1":
+    plik = input("Podaj nazwe pliku do skompresowania:\n")
+    kompresator.kompresja(plik)
+elif opcja == "2":
+    nowy_plik = input("Podaj nowa nazwe zdekompresowanego pliku:\n")
+    kody = input("Podaj nazwe pliku z kodem:\n")
+    paczka = input("Podaj nazwe zakodowanego pliku:\n")
+    kompresator.dekompresja(nowy_plik, kody, paczka)
+elif opcja == "0":
+    exit()
